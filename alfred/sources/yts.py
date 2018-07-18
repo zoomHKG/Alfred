4f50168cd285638151a94aa9c60e0854484bee2c
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-18 21:17:59 +0200

    added boilerplate for yts

--- alfred/sources/yts.py
+++ alfred/sources/yts.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """yts.ag scrapping stuffs here"""
+
+class YTS():
+    """Class for YTS scrapping"""
+
+
+    def get_latest(self):
+        """fetch latest uploads from yts"""
+        # TODO: implement scrapping
+        return []
+
+
+    def get_featured(self):
+        """fetch featured movies from yts"""
+        # TODO: implement scrapping
+        return []
