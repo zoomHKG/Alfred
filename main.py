f1a981631c28f2ac1cde92d2bab0cfd82564e7a7
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-19 10:41:08 +0200

    tornado web endpoint boilerplate

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 """A bot to monitor YTS.ag for new movies"""
 import sys
 import configparser
+from tornado.ioloop import IOLoop
+from alfred.server import make_app
 from alfred.repo import Repository
 from alfred.util import logger
 
@@ -20,6 +22,10 @@ def main():
     repo_url = config['REPOSITORY']['URL']
     repo = Repository(repo_url)
     logger.debug(repo.get_url())
+
+    app = make_app()
+    app.listen(8888)
+    IOLoop.current().start()
 
 if __name__ == '__main__':
     main()
