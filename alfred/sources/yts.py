03d8578c9bc6f04c08dd09e3cd75dd6351d7926f
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-19 20:59:39 +0200

    cleanup

--- alfred/sources/yts.py
+++ alfred/sources/yts.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
+"""yts.ag scrapping stuffs here"""
+import os
 import requests
 from bs4 import BeautifulSoup
-
-"""yts.ag scrapping stuffs here"""
 
 
 class YTS():
     """Class for YTS scrapping"""
 
     def __init__(self):
-        self.crawlUrl = 'https://yts.am/'
+        """Constructor"""
+        self.url = os.environ.get("YTS", 'https://yts.am/')
+
+
+    def get_page(self):
+        """fetch and parse yts homepage"""
+        source_code = requests.get(self.url)
+        plain_text = source_code.text
+        return BeautifulSoup(plain_text, "html.parser")
+
 
     def get_latest(self):
+        """get latest movies"""
         latest_movies = []
-        source_code = requests.get(self.crawlUrl)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text, "html.parser")
+        soup = self.get_page()
         latest_parent = soup.find('div', {'class': 'home-movies'})
         latest_child_div = latest_parent.findAll('div', {'class': 'browse-movie-wrap'})
-        for l in latest_child_div:
-            movie_name_div = l.find('div', {'class': 'browse-movie-bottom'})
+        for elem in latest_child_div:
+            movie_name_div = elem.find('div', {'class': 'browse-movie-bottom'})
             movie_name = movie_name_div.find('a', {'class': 'browse-movie-title'}).text
             latest_movies.append(movie_name)
         return latest_movies
 
+
     def get_featured(self):
+        """get featured movies"""
         featured_movies = []
-        source_code = requests.get(self.crawlUrl)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text, "html.parser")
+        soup = self.get_page()
         featured_parent = soup.find('div', {'id': 'popular-downloads'})
         featured_child_div = featured_parent.findAll('div', {'class': 'browse-movie-wrap'})
-        for f in featured_child_div:
-            movie_name_div = f.find('div', {'class': 'browse-movie-bottom'})
+        for elem in featured_child_div:
+            movie_name_div = elem.find('div', {'class': 'browse-movie-bottom'})
             movie_name = movie_name_div.find('a', {'class': 'browse-movie-title'}).text
             featured_movies.append(movie_name)
         return featured_movies
